119ad467a8fbdb92cd6a3d6e774dec578d63d0b0
ABJ135 <1515.abubakarjaved.m2@gmail.com>
2024-11-09 12:34:39 +0500

    delete request

--- index.js
+++ index.js
@@ -22,73 +22,92 @@ app.post("/add", (req, res) => {
 });
 
 //This is a get request
-app.get("/data",(req,res)=>{
-  fs.readFile('abc.json', (err,data)=>{
-    if(err){
-      console.log(err)
-    }
-    else{
-      res.json(JSON.parse(data))
-    }
-  })
-})
-
-//This is a get request with query
-app.get('/data/search',(req,res)=>{
-  const search = req.query.name;
-  fs.readFile('abc.json',(err,data)=>{
-    const arr = JSON.parse(data);
-
-    const foundItem = arr.filter(item=> item.name.toLowerCase().includes(search.toLowerCase()))
-
-    res.json(foundItem)
-  })
-})
-
-//This is a get request with params
-app.get('/data/:id',(req,res)=>{
-  const id =parseInt(req.params.id);
-  fs.readFile('abc.json',(err,data)=>{
-    if(err){
-      res.json('File error')
-    }
-    else{
-      const arr = JSON.parse(data)
-      const foundItem = arr.find(item=>item.id === id)
-      res.json(foundItem)
-    }
-  })
-})
-
-//This is put request
-app.put('/data/:id',(req,res)=>{
-const id = parseInt(req.params.id);
-const item = req.body;
-
-
-fs.readFile('abc.json',(err,data)=>{
-  if(err){
-    res.json("File error");
-  }
-  
-    let arr = JSON.parse(data);
-
-    arr[id-1]=item;
-  
-
-  fs.writeFile('abc.json',JSON.stringify(arr),(err)=>{
-    if(err){
-      console.log('Error writing in file')
-    }
-    else{
-    //  res.send("Data written successfully")
-      res.json(arr);
+app.get("/data", (req, res) => {
+  fs.readFile("abc.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(JSON.parse(data));
     }
   });
 });
+
+//This is a get request with query
+app.get("/data/search", (req, res) => {
+  const search = req.query.name;
+  fs.readFile("abc.json", (err, data) => {
+    const arr = JSON.parse(data);
+
+    const foundItem = arr.filter((item) =>
+      item.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+    res.json(foundItem);
+  });
+});
+
+//This is a get request with params
+app.get("/data/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  fs.readFile("abc.json", (err, data) => {
+    if (err) {
+      res.json("File error");
+    } else {
+      const arr = JSON.parse(data);
+      const foundItem = arr.find((item) => item.id === id);
+      res.json(foundItem);
+    }
+  });
+});
+
+//This is put request
+app.put("/data/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const item = req.body;
+
+  fs.readFile("abc.json", (err, data) => {
+    if (err) {
+      res.json("File error");
+    }
+
+    let arr = JSON.parse(data);
+
+    arr[id - 1] = item;
+
+    fs.writeFile("abc.json", JSON.stringify(arr), (err) => {
+      if (err) {
+        console.log("Error writing in file");
+      } else {
+        //  res.send("Data written successfully")
+        res.json(arr);
+      }
+    });
+  });
 });
 
 //This is a delete request
+app.delete("/data/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  let v = id - 1;
+  fs.readFile("abc.json", (err, data) => {
+    if (err) {
+      res.json("File error");
+    }
+
+    let arr = JSON.parse(data);
+
+    arr.splice(v, 1);
+
+    fs.writeFile("abc.json", JSON.stringify(arr), (err) => {
+      if (err) {
+        console.log("Error writing in file");
+      } else {
+        //  res.send("Data written successfully")
+        res.json(arr);
+      }
+    });
+  });
+});
 
 app.listen(port, (req, res) => {
   console.log(`server is running at port ${port}`);
